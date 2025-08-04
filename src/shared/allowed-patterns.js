8404ff55ab8e35c53316a51230142df00efe1452
Jacob Tender <jacob.tender@gmail.com>
2025-08-04 11:54:47 -0400

    Add The StoryGraph urls to whitelist

--- src/shared/allowed-patterns.js
+++ src/shared/allowed-patterns.js
@@ -2,7 +2,8 @@
 const ALLOWED_PATTERNS = [
   /https:\/\/www\.amazon\.com\/gp\/product\/[A-Z0-9]{10}/,
   /https:\/\/www\.amazon\.com\/[^/]+\/dp\/[A-Z0-9]{10}/,
-  /https:\/\/www\.goodreads\.com\/book\/show\/\d+(-[a-zA-Z0-9-]+)?/
+  /https:\/\/www\.goodreads\.com\/book\/show\/\d+(-[a-zA-Z0-9-]+)?/,
+  /https:\/\/app\.thestorygraph\.com\/books\/[a-f0-9-]{36}(\/editions)?/
 ];
 
 function isAllowedUrl(url) {
