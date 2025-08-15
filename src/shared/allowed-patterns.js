88a2b8debd2d7681c1e92123a7e1973ef9ec5813
Michael Cummings <3028512+kodermike@users.noreply.github.com>
2025-08-15 11:12:21 -0700

    issue-33 - kobo support

--- src/shared/allowed-patterns.js
+++ src/shared/allowed-patterns.js
@@ -5,7 +5,8 @@ const ALLOWED_PATTERNS = [
   /https:\/\/www\.amazon\.[a-z.]+\/-\/[a-z]+\/[^/]+\/dp\/[A-Z0-9]{10}/, // for paths with language segments
   /https:\/\/www\.goodreads\.[a-z.]+\/book\/show\/\d+(-[a-zA-Z0-9-]+)?/,
   /^https:\/\/app\.thestorygraph\.[a-z.]+\/books\/[0-9a-fA-F-]+$/,
-  /^https?:\/\/(www\.)?google\.[a-z.]+\/books/
+  /^https?:\/\/(www\.)?google\.[a-z.]+\/books/,
+  /^https?:\/\/(www\.)?kobo\.[a-z]{2,10}\/[a-z]{2,5}\/[a-z]{2,5}\/[a-z]{1,5}book\/[0-9a-z\-]+/
 ];
 
 export function isAllowedUrl(url) {
