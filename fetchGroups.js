49f3d82d4456076301e4689e14cc012b3e051ab8
zivoy <ziv132@gmail.com>
2025-09-30 01:30:12 -0400

    Get from internet

--- fetchGroups.js
+++ fetchGroups.js
@@ -3,12 +3,43 @@ const fs = require("fs/promises");
 
 const parseString = (str) => new Promise((resolve, reject) => xml2js.parseString(str, (err, result) => err != null ? reject(err) : resolve(result)));
 
-// TODO: fetch from link
+// Get the Range Message xml file from https://www.isbn-international.org/range_file_generation
+async function getFileUrl() {
+  const domain = 'https://www.isbn-international.org';
 
-fs.readFile("./RangeMessage.xml", "utf8")
+  const resp = await fetch(`${domain}/bl_proxy/GetRangeInformations`, {
+    method: 'POST',
+    body: new URLSearchParams({
+      format: 1,
+      language: 'en',
+      translatedTexts: 'Printed;Last Change'
+    })
+  });
+  const json = await resp.json();
+  if (json["status"] !== "success") {
+    console.error("Error generating url", json["messages"]);
+    throw "Couldn't make url";
+  }
+
+  const { filename, value } = json.result;
+  return `${domain}/download_range/${value}/${filename}`;
+}
+
+const filepath = ""; // "./RangeMessage.xml";
+
+let start;
+if (filepath) {
+  start = fs.readFile(filepath, "utf8");
+} else {
+  start = getFileUrl()
+    .then(fetch)
+    .then(resp => resp.text())
+    .catch(console.error)
+}
+
+start
   .then(parseString)
   .then(json => json["ISBNRangeMessage"]["RegistrationGroups"][0]["Group"]) // get groups
-  // .then(groups => groups.map(group => [group["Prefix"][0], group["Agency"][0]])) // get a list of tuples of prefix and group name
   .then(groups => groups.map(group => ({ [group["Prefix"][0]]: group["Agency"][0] }))) // get a list of maps of prefix to group name
   .then(groups => groups.reduce((acc, obj) => ({ ...acc, ...obj }), {})) // flatten it
   .then(list => JSON.stringify(list, undefined, 2))
