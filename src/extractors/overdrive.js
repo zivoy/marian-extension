4f27f77db0ab0fb65109b9a67569197835c68c68
zivoy <ziv132@gmail.com>
2025-10-12 03:09:27 -0400

    Basic parsing

--- src/extractors/overdrive.js
+++ src/extractors/overdrive.js
@@ -2,7 +2,7 @@ import { getImageScore, logMarian, sendMessage, getFormattedText } from '../shar
 
 async function getOverdriveDetails() {
   const id = document.querySelector(".cover img")?.dataset?.id;
-  if (id == undefined) return {};
+  if (id == undefined) throw "Id not found";
   return getDetailsFromOverdriveId(id);
 }
 
@@ -21,12 +21,77 @@ async function getLibbyDetails() {
     }
   }
 
-  if (id == undefined) return {};
+  if (id == undefined) throw "Id not found";
   return getDetailsFromOverdriveId(id);
 }
 
+const apiUrl = "https://thunder.api.overdrive.com/v2/media/bulk";
+
 async function getDetailsFromOverdriveId(id) {
-  throw "Not implemented"
+  // retrieve data
+  const url = new URL(apiUrl);
+  url.searchParams.set('titleIds', id);
+  url.searchParams.set('x-client-id', "marian"); // dewey
+
+  const req = await fetch(url);
+  if (!req.ok) {
+    throw "Failed to retrive data from api";  // failed to retrieve data
+  }
+  let data = await req.json();
+  if (data.length < 1) {
+    throw "Data not found"
+  }
+  data = data[0];
+
+  logMarian("book data", data);
+
+  const details = {};
+
+  // Get relevant details
+  // cover
+  const cover = Object.values(data.covers)
+    .reduce((cover, { width, height, href }) => cover.res > (width * height) ? cover : { res: width * height, href }) // get cover with highest resolution
+  details["img"] = cover.href;
+  details["imgScore"] = cover.res; // save on calling getImageScore because we already have the resolution
+
+  // contributors
+  const contributors = Object.values(data.creators.reduce((acc, { id, name, role }) => {
+    if (!acc[id]) {
+      acc[id] = { name, roles: [] };
+    }
+    acc[id].roles.push(role);
+    return acc;
+  }, {}));
+  details["Contributors"] = contributors;
+
+  // description
+  details["Description"] = getFormattedText(data.fullDescription);
+
+  // title
+  let title = data.title;
+  if (data.subtitle) {
+    title = title + ": " + data.subtitle
+  }
+  details["Title"] = title
+
+  // format
+  details["Edition Format"] = data.type?.name; // eBook most of the time
+
+  // publisher
+  details["Publisher"] = data.publisher?.name;
+  details["Publisher Account"] = data.publisherAccount?.name; // ??
+
+  details["Imprint"] = data.imprint?.name;
+
+  // date
+  details["Publication date"] = data.publishDate ?? data.estimatedReleaseDate;
+
+  // Format data
+
+
+  logMarian("book details", details);
+
+  return details;
 }
 
 export { getOverdriveDetails, getLibbyDetails };
