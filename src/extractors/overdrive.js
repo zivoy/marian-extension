8490494a81bd652358300c37442735911cf31038
zivoy <ziv132@gmail.com>
2025-10-12 16:05:33 -0400

    audiobook foramt

--- src/extractors/overdrive.js
+++ src/extractors/overdrive.js
@@ -187,8 +187,6 @@ function collapseEbookFormats(formats) {
     if (formatInfo["Edition Format"] == undefined) {
       formatInfo["Edition Format"] = format.name;
     }
-
-    // formatList[formatId] = formatInfo;
   }
 
   formatList.forEach((value) => {
@@ -198,6 +196,72 @@ function collapseEbookFormats(formats) {
   return formatList
 }
 function collapseAudiobookFormats(formats) {
+  const formatList = [];
+  for (let format of formats) {
+    let formatInfo = {};
+    let formatId = -1;
+
+    // match by date
+    let match = formatList.findIndex((el) => el.saleDate == format.onSaleDateUtc);
+    if (match != -1 && formatId == -1) {
+      formatId = match;
+      formatInfo = formatList[formatId];
+    }
+    // match by duration
+    match = formatList.findIndex((el) => el.duration == format.duration);
+    if (match != -1 && formatId == -1) {
+      formatId = match;
+      formatInfo = formatList[formatId];
+    }
+    // match by isbn
+    match = formatList.findIndex((el) => el["ISBN-13"] == format.isbn || el["ISBN-10"] == format.isbn);
+    if (match != -1 && formatId == -1) {
+      formatId = match;
+      formatInfo = formatList[formatId];
+    }
+
+    // treat it as a new entry if not matched
+    if (formatId == -1) {
+      formatId = formatList.length;
+      formatList.push(formatInfo);
+    }
+
+    // --
+
+    if (formatInfo.saleDate == undefined) {
+      formatInfo.saleDate = format.onSaleDateUtc;
+    }
+
+    // NOTE: note sure about this, audiobooks don't typically have ISBNs
+    if (format.isbn?.length == 13) {
+      formatInfo["ISBN-13"] = format.isbn;
+    } else if (format.isbn?.length == 10) {
+      formatInfo["ISBN-10"] = format.isbn;
+    }
+
+    if (formatInfo["Edition Format"] == undefined) {
+      formatInfo["Edition Format"] = format.name;
+    }
+
+    if (formatInfo["Listening Length"] == undefined) {
+      const duration = format.duration.split(":").map((val) => parseInt(val));
+      if (duration.length == 3) {
+        const [hours, minutes, seconds] = duration;
+        formatInfo["Listening Length"] = [hours + " Hours", minutes + " Minutes", seconds + " Seconds"];
+      } else if (duration.length == 2) {
+        const [hours, minutes] = duration;
+        formatInfo["Listening Length"] = [hours + " Hours", minutes + " Minutes"];
+      } else {
+        logMarian(`WARN: unknown duration ${format.duration}`)
+      }
+    }
+  }
+
+  formatList.forEach((value) => {
+    delete value["saleDate"]; // not needed
+  });
+
+  return formatList
 }
 
 export { getOverdriveDetails, getLibbyDetails };
