a9f0fb4a8f83dc328bef395f44409b9380dacac9
zivoy <ziv132@gmail.com>
2025-12-26 02:36:41 -0500

    Implement bookshop.org

---
+++ src/extractors/bookshopOrg.js
@@ -0,0 +1,87 @@
+import { addContributor, cleanText, collectObject, getCoverData, getFormattedText, normalizeReadingFormat } from "../shared/utils.js";
+import { Extractor } from "./AbstractExtractor.js"
+
+class bookshopScraper extends Extractor {
+  get _name() { return "bookstore.org Extractor"; }
+
+  needsReload = false;
+
+  _sitePatterns = [
+    /https:\/\/bookshop\.org\/p\/books\/(.+)/,
+  ];
+
+  async getDetails() {
+    let details = {};
+
+    const ldInfoJsonEl = document.querySelector("script[type='application/ld+json']");
+    const ldInfo = JSON.parse(ldInfoJsonEl.textContent);
+    console.log("info", ldInfo);
+
+    // cover
+    const coverData = getCoverData(ldInfo["image"].filter(i => !i.includes("?v=enc-v1")));
+
+    // isbn
+    if (ldInfo["isbn"].length === 13) {
+      details["ISBN-13"] = ldInfo["isbn"];
+    } else {
+      if (ldInfo["isbn"].length === 10) {
+        details["ISBN-10"] = ldInfo["isbn"];
+      }
+    }
+
+    // description
+    const description = document.createElement("div");
+    description.innerHTML = ldInfo["description"];
+    details["Description"] = getFormattedText(description);
+
+    // date
+    // details["Publication date"] = ldInfo["datePublished"];
+    details["Publication date"] = document.querySelector("meta[name='book:release_date']").content;
+
+    // title
+    details["Title"] = ldInfo["name"];
+    const subtitle = document.querySelector("meta[name='description']").content;
+    if (subtitle) {
+      details["Title"] = `${details["Title"]}: ${subtitle}`;
+    }
+
+    // format
+    details["Edition Format"] = ldInfo["bookFormat"];
+    details["Reading Format"] = normalizeReadingFormat(ldInfo["bookFormat"]);
+
+    // length
+    if ("numberOfPages" in ldInfo && ldInfo["numberOfPages"] > 0) {
+      details["Pages"] = ldInfo["numberOfPages"];
+    }
+
+    // author(s)
+    let contributors = [];
+    for (const author of ldInfo["author"]) {
+      if (author && author.name) {
+        addContributor(contributors, author.name, "Author");
+      }
+    }
+    details["Contributors"] = contributors;
+
+    // publisher
+    details["Publisher"] = ldInfo["publisher"]?.name;
+
+    // language (and potentially other fields)
+    const table = document.querySelector("details table");
+    for (const row of table.querySelectorAll("tr")) {
+      const [key, val] = row.children;
+      const keyname = cleanText(key.textContent);
+      const value = cleanText(val.textContent);
+      if (keyname === "Language") {
+        details["Language"] = value;
+      }
+    }
+
+    return collectObject([
+      details,
+      coverData
+    ]);
+  }
+}
+
+export { bookshopScraper };
--- src/extractors/index.js
+++ src/extractors/index.js
@@ -2,6 +2,7 @@
 import { amazonScraper } from "./amazon";
 import { audibleScraper } from "./audible";
 import { barnesAndNobleScraper } from "./barnesnoble";
+import { bookshopScraper } from "./bookshopOrg";
 import { dnbdeScraper } from "./dnbde";
 import { goodreadsScraper } from "./goodreads";
 import { googleBooksScraper } from "./googlebooks";
@@ -19,6 +20,7 @@ const extractors = [
   new amazonScraper(),
   new audibleScraper(),
   new barnesAndNobleScraper(),
+  new bookshopScraper(),
   new dnbdeScraper(),
   new goodreadsScraper(),
   new googleBooksScraper(),
