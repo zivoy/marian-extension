04f2ebfde8842602db59ef26faca2175a3b66032
zivoy <ziv132@gmail.com>
2026-02-02 00:41:18 -0500

    fix date not showing correctly

--- src/popup/utils.js
+++ src/popup/utils.js
@@ -215,13 +215,13 @@ export function normalizeDetails(details, settings, inplace = true) {
 export function formatDate(dateStr, format = "local") {
   const date = new Date(dateStr);
 
-  if (isNaN(date)) {
+  if (Number.isNaN(date.getTime())) {
     return dateStr;
   }
 
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
 
   switch (format) {
     case 'ymd':
